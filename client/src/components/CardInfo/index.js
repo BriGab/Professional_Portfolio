9fd5d0876f769ddc4f23ab5833fd947adcd6a5e2
Bri Gab <brigabiola@outlook.com>
2020-05-12 13:43:19 -0600

    adjust link logic

--- client/src/components/CardInfo/index.js
+++ client/src/components/CardInfo/index.js
@@ -4,11 +4,9 @@ import "./style.css"
 
 function CardInfo (props) {
 
-
-  
     return (<>
       <Carousel.Caption id="words">
-        <a className="carLinks" href={!props.githubPages ? props.heroku : props.githubPages} rel="noopener noreferrer" target="_blank"><h3>{props.title}</h3></a>
+        <a className="carLinks" href={props.githubPages ? props.githubPages : props.heroku} rel="noopener noreferrer" target="_blank"><h3>{props.title}</h3></a>
         <a className="carLinks" href={props.githubRepo} rel="noopener noreferrer" target="_blank"> <i id="gitLinks" className="fa fa-github"></i></a>
           <p id="description">{props.description}</p>
         </Carousel.Caption>
