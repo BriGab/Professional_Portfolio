da4c6b9723b887441af5ee3fce9e16aec0916c5f
Bri Gab <brigabiola@outlook.com>
2020-05-14 17:00:31 -0600

    updated prev & next icons

--- client/src/components/CardImage/index.js
+++ client/src/components/CardImage/index.js
@@ -5,7 +5,7 @@ import "./style.css"
 function CardImage (props) {
     return (<>
             <center><img
-                className="d-block w-100"
+                className="d-block w-100 cardStyle"
                 src={props.image}
                 alt={props.title}
             /></center> 
